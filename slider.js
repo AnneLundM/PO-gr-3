f5a54b4c5f7a98a37b4367a20f17a686e8c69762
Sebastian Jensen <sebastianbjensen@gmail.com>
2026-05-28 09:04:36 +0200

    Add: Hero-sektion med slider.

---
+++ slider.js
@@ -0,0 +1,27 @@
+﻿const images = [
+  "assets/headerslider/01.jpg",
+  "assets/headerslider/02.jpg",
+  "assets/headerslider/03.jpg",
+  "assets/headerslider/04.jpg",
+];
+
+let currentIndex = 0;
+const slideImage = document.getElementById("slideImage");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+prevButton.addEventListener("click", () => {
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  slideImage.src = images[currentIndex];
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex += 1;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  slideImage.src = images[currentIndex];
+});
